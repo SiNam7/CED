cdf2e97d529785bd5bf8709cd89a72f5bf05e422
SiNam7 <muwon777@naver.com>
2020-09-17 10:20:56 +0900

    05P12

---
+++ 05_practice_2.ino
@@ -0,0 +1,20 @@
+#define PIN_LED 7
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(PIN_LED, 0);
+  delay(1000);
+
+  int t = 5;
+  while (t--) {
+    digitalWrite(PIN_LED, 1);
+    delay(100);
+    digitalWrite(PIN_LED, 0);
+    delay(100);  
+  }
+  
+  digitalWrite(PIN_LED, 1);
+  while (true);
+}
